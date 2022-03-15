9b3ecaaacce990575996968f006be3951f7bab11
gracekabaghe <gracema2002@gmail.com>
2022-03-15 18:04:43 +0200

    Updated recipes

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -1,3 +1,10 @@
 class GeneralController < ActionController::Base
-  def index; end
+  def index
+    @user = current_user
+    @recipe = Recipe.where(user_id: @user.id)
+    @recipe_foods = RecipeFood.all
+    @foods = Food.where(user_id: @user.id)
+    @recipes = @user.recipes
+    @total = @recipes.map { |x| Recipe.total_value(x.id) }.reduce(:+)
+ end
 end
